4a6c68210004074a391eddb4029bf078faca5aa8
Hugos68 <hugokorteapple@gmail.com>
2023-10-10 00:22:06 +0200

    Implemented docs layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<header
+	class="sticky top-0 p-4 bg-stone-800 px-[clamp(1rem,10vw,25rem)] h-[var(--header-height)] flex items-center z-50"
+>
+	<p class="font-semibold text-lg">Action Archive</p>
+</header>
+
+<main class="flex px-[clamp(1rem,10vw,25rem)]">
+	<aside
+		class="sticky top-[var(--header-height)] h-[calc(100vh-calc(var(--header-height)))] overflow-auto w-1/5 flex flex-col py-8"
+	>
+		<p class="text-lg font-semibold">Overview</p>
+		<span class="flex flex-col gap-1 mt-1.5">
+			<a class="ml-2" href="/overview/introduction">Introduction</a>
+			<a class="ml-2" href="/overview/getting-started">Getting started</a>
+		</span>
+		<p class="text-lg font-semibold mt-6">Actions</p>
+		<span class="flex flex-col gap-1 mt-1.5">
+			<a href="/actions/button" class="ml-2">Button</a>
+			<a href="/actions/click-outside" class="ml-2">Click Outside</a>
+			<a href="/actions/focus-trap" class="ml-2">Focus Trap</a>
+			<a href="/actions/portal" class="ml-2">Portal</a>
+		</span>
+	</aside>
+	<div class="w-3/5 overflow-y-auto px-4 py-8">
+		<slot />
+	</div>
+	<aside class="w-1/5 flex flex-col px-4 py-8">
+		<!-- TODO: TOC -->
+	</aside>
+</main>
+
+<footer />
---
+++ src/routes/overview/introduction/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { ArrowRightIcon } from 'svelte-feather-icons';
+</script>
+
+<p class="opacity-50">Overview</p>
+<h1 class="text-4xl mt-4">Introduction</h1>
+<p class="mt-4">
+	Action Archive is a collection of actions for Svelte aimed to reduce the amount of boilerplate
+	throughout projects.
+</p>
+
+<h2 class="text-2xl mt-12">Motivation</h2>
+<p class="mt-4">
+	I've been using Svelte for a while now and I've noticed that I've been writing the same actions
+	over and over again. This motivated me to write a collection of commonly used actions and bundle
+	them into a single package.
+</p>
+
+<h2 class="text-2xl mt-12">Inspiration</h2>
+<p class="mt-4">
+	I want to credit both Skeleton UI and Melt UI as they inspired some actions.
+	<span class="block">
+		- <a class="underline font-bold" href="https://www.skeleton.dev/">Skeleton UI</a>
+	</span>
+	<span class="block">
+		- <a class="underline font-bold" href="https://www.melt-ui.com/">Melt UI</a>
+	</span>
+</p>
+
+<div class="flex justify-end mt-12">
+	<a class="hover:underline" href="/overview/getting-started"
+		>Getting Started <ArrowRightIcon class="inline" /></a
+	>
+</div>
