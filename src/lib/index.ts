ff149e204f7223bc1d3a8de149907346104dfa63
Hugo <hugokorteapple@gmail.com>
2023-10-10 23:27:12 +0200

    fixed packaging

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
-export * from './actions/button/button.js';
-export * from './actions/click_outside/click_outside.js';
-export * from './actions/focus_trap/focus_trap.js';
-export * from './actions/portal/portal.js';
+export { button } from './actions/button/button.js';
+export { click_outside } from './actions/click_outside/click_outside.js';
+export { focus_trap } from './actions/focus_trap/focus_trap.js';
+export { portal } from './actions/portal/portal.js';
