22ddd5ed5669b0b227630d8d5447d39ea1cb1853
Hugo <hugokorteapple@gmail.com>
2023-10-14 18:13:20 +0200

    Improved button example

--- src/routes/actions/button/ButtonExample.svelte
+++ src/routes/actions/button/ButtonExample.svelte
@@ -2,8 +2,8 @@
 	import { button } from '$lib';
 </script>
 
-<div class="py-32">
-	<a use:button href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" class="btn"
+<div class="py-32 flex justify-center items-center px-8">
+	<a use:button href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" class="btn text-center"
 		>I'm an anchor tag that won't take you anywhere!
 	</a>
 </div>
