d84c1a14c44f8abe4e46b53746d38f104fe7ea00
Hugo <hugokorteapple@gmail.com>
2023-10-16 23:44:15 +0200

    Rewrote intersect action

---
+++ .changeset/fluffy-clouds-accept.md
@@ -0,0 +1,5 @@
+---
+'action-archive': patch
+---
+
+Internal rewrite for intersect action, implemented suggestion from: #36
--- src/lib/actions/intersect/index.ts
+++ src/lib/actions/intersect/index.ts
@@ -4,15 +4,18 @@ import { emit } from '../../internal/emit.js';
 
 export function intersect(
 	node: HTMLElement,
-	{ root = null, rootMargin = '0px 0px 0px 0px', threshold = [0] }: IntersectParameters
+	{ root = null, rootMargin = '0px 0px 0px 0px', threshold = [0] }: IntersectParameters = {}
 ): ActionReturn<IntersectParameters, IntersectEvents> {
-	let intersectionObserver = new IntersectionObserver(onIntersect, { root, rootMargin, threshold });
-
 	function onIntersect(entries: IntersectionObserverEntry[]) {
-		const entry = entries[0];
-		emit(node, 'intersect', { entry });
-		if (entries[0].isIntersecting) emit(node, 'enter', { entry });
-		if (!entries[0].isIntersecting) emit(node, 'leave', { entry });
+		for (const entry of entries) {
+			emit(entry.target as HTMLElement, 'intersect', { entry });
+			emit(entry.target as HTMLElement, entry.isIntersecting ? 'enter' : 'leave', { entry });
+		}
+	}
+
+	function init() {
+		intersectionObserver = registerObserver(onIntersect, { root, rootMargin, threshold });
+		intersectionObserver.observe(node);
 	}
 
 	function update({
@@ -20,19 +23,60 @@ export function intersect(
 		rootMargin: newRootMargin = '0px 0px 0px 0px',
 		threshold: newThreshold = [0]
 	}: IntersectParameters) {
+		destroy();
 		root = newRoot;
 		rootMargin = newRootMargin;
 		threshold = newThreshold;
-		intersectionObserver.disconnect();
-		intersectionObserver = new IntersectionObserver(onIntersect, { root, rootMargin, threshold });
-		intersectionObserver.observe(node);
+		init();
 	}
 
 	function destroy() {
-		intersectionObserver.disconnect();
+		intersectionObserver.unobserve(node);
+		unregisterObserver({ root, rootMargin, threshold });
 	}
 
-	intersectionObserver.observe(node);
+	let intersectionObserver: IntersectionObserver;
+	init();
 
 	return { update, destroy };
+}
+
+/**
+ * This map is used to keep track of what IntersectionObserver are already created with each set of parameters.
+ * This ensures that actions with the same set of params use the same observer rather can creating a new one.
+ * This majorly improves performance when dealing with thousands of observed nodes.
+ * Related issue: https://github.com/Hugos68/action-archive/issues/36
+ */
+const stringifiedParametersObserverMap = new Map<
+	string,
+	{ users: number; intersectionObserver: IntersectionObserver }
+>();
+
+function registerObserver(
+	onIntersect: IntersectionObserverCallback,
+	{ root, rootMargin, threshold }: IntersectParameters
+) {
+	const stringifiedParameters = JSON.stringify({ root, rootMargin, threshold });
+	const value =
+		stringifiedParametersObserverMap.get(stringifiedParameters) ||
+		(() => {
+			const newValue = {
+				users: 0,
+				intersectionObserver: new IntersectionObserver(onIntersect, { root, rootMargin, threshold })
+			};
+			stringifiedParametersObserverMap.set(stringifiedParameters, newValue);
+			return newValue;
+		})();
+	value.users++;
+	return value.intersectionObserver;
+}
+
+function unregisterObserver({ root, rootMargin, threshold }: IntersectParameters) {
+	const stringifiedParameters = JSON.stringify({ root, rootMargin, threshold });
+	const value = stringifiedParametersObserverMap.get(stringifiedParameters);
+	if (!value) return;
+	value.users--;
+	if (value.users > 0) return;
+	value.intersectionObserver.disconnect();
+	stringifiedParametersObserverMap.delete(stringifiedParameters);
 }
