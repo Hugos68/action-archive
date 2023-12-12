aef579c426215c8ba796324acfd8189201d65750
André de Oliveira Brandão <brandaoandre00@gmail.com>
2023-12-12 19:40:26 -0300

    money mask

---
+++ src/routes/(docs)/actions/mask/MaskExampleMoney.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { mask } from 'action-archive';
+
+	let value: string = 'money';
+</script>
+	<!-- svelte-ignore a11y-label-has-associated-control -->
+
+<div class="py-32 px-8 flex flex-col gap-4 justify-center items-center">
+	<label class="flex flex-col">
+		Mask: 'money'
+	</label>
+	<label class="flex flex-col">
+		Money mask:
+		<input class="input max-w-xs" value={0.00} placeholder="0.00" use:mask={{ mask: value }} />
+	</label>
+</div>
